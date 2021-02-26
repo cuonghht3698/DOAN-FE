6fab15614332412bc74a64912bf04fae72fa77f0
Cuong <Cuong@DESKTOP-3LF9ATD>
2021-02-26 20:27:04 +0700

    a

--- src/app/components/admin/directives/sanpham-option/sanpham-option.component.ts
+++ src/app/components/admin/directives/sanpham-option/sanpham-option.component.ts
@@ -2,12 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { SanPhamService } from 'src/app/services/danhmuc/sanpham.service';
 
-export interface State {
-  flag: string;
-  name: string;
-  population: string;
-}
 @Component({
   selector: 'app-sanpham-option',
   templateUrl: './sanpham-option.component.html',
@@ -15,15 +11,11 @@ export interface State {
 })
 export class SanphamOptionComponent implements OnInit {
   stateCtrl = new FormControl();
-  filteredStates: Observable<State[]>;
-  constructor() {
-    this.filteredStates = this.stateCtrl.valueChanges
-      .pipe(
-        startWith(''),
-        map(state => state ? this._filterStates(state) : this.states.slice())
-      );
+  filteredStates: Observable<any>;
+  constructor(private sp: SanPhamService) {
+    
   }
-  private _filterStates(value: string): State[] {
+  private _filterStates(value: string): any{
     const filterValue = value.toLowerCase();
 
     return this.states.filter(state => state.name.toLowerCase().indexOf(filterValue) === 0);
@@ -31,13 +23,23 @@ export class SanphamOptionComponent implements OnInit {
   @Input() demo: any;
   @Input() viewNhap: boolean;
   ngOnInit(): void {
+    this.getProduct();
   }
-  states: State[] = [
-    {
-      name: '218312837123123',
-      population: '2.978M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
-    }
-  ];
+  states: any;
+  Search = {
+    sSearch:'',
+    pageIndex:1,
+    pageSize : 10
+  }
+  getProduct(){
+    this.sp.GetPage(this.Search).subscribe((res: any)=>{
+      this.states = res.List;
+      console.log(res);
+      this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => state ? this._filterStates(state) : this.states.slice())
+      );
+    })
+  }
 }
