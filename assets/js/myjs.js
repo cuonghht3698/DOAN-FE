3ea7a12b09b20264759cdf70972e6a2c63acfb16
Cuong <Cuong@DESKTOP-3LF9ATD>
2021-03-14 12:05:06 +0700

    Auto-generated commit

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,62 +1,62 @@
-$(document).ready(function() {
-    $(".product-cart").hide();
-    $(".dropdown-menu ").hide();
-    $(".product").hover(function() {
-        $(this).find(".hang").animate({
-            'marginTop': "-68px"
-        });
-        $(this).find(".product-cart").toggle();
+// $(document).ready(function() {
+//     $(".product-cart").hide();
+//     $(".dropdown-menu ").hide();
+//     $(".product").hover(function() {
+//         $(this).find(".hang").animate({
+//             'marginTop': "-68px"
+//         });
+//         $(this).find(".product-cart").toggle();
 
-    }, function() {
-        $(this).find(".hang").animate({
-            'marginTop': "-30px"
-        });
+//     }, function() {
+//         $(this).find(".hang").animate({
+//             'marginTop': "-30px"
+//         });
 
-        $(this).find(".product-cart").toggle();
-    });
+//         $(this).find(".product-cart").toggle();
+//     });
 
-    $(".btn-group ").hover(function() {
-        $(this).find(".dropdown-menu ").show();
-
-
-    }, function() {
-        $(".dropdown-menu ").hide();
-    });
+//     $(".btn-group ").hover(function() {
+//         $(this).find(".dropdown-menu ").show();
 
 
+//     }, function() {
+//         $(".dropdown-menu ").hide();
+//     });
 
 
-});
-
-$(".top-selling .next").click(function() {
 
 
-    var check = parseInt($(".product-mid").css("left"));
+// });
+
+// $(".top-selling .next").click(function() {
 
 
-    if (check > -1400)
-        $(".product-mid").animate({
-            left: "-=400"
-        });
-    else {
-        $(".product-mid").css({
-            "left": "-1315px"
-        });
-    }
-});
-$(".top-selling .prev").click(function() {
-    var check = parseInt($(".product-mid").css("left"));
-    if (check < 0)
-        $(".product-mid").animate({
-            left: "+=400"
-        });
-    else {
-        $(".product-mid").css({
-            "left": "15px"
-        });
-    }
-});
+//     var check = parseInt($(".product-mid").css("left"));
 
-$('.carousel').carousel({
-    interval: 2000
-  })
+
+//     if (check > -1400)
+//         $(".product-mid").animate({
+//             left: "-=400"
+//         });
+//     else {
+//         $(".product-mid").css({
+//             "left": "-1315px"
+//         });
+//     }
+// });
+// $(".top-selling .prev").click(function() {
+//     var check = parseInt($(".product-mid").css("left"));
+//     if (check < 0)
+//         $(".product-mid").animate({
+//             left: "+=400"
+//         });
+//     else {
+//         $(".product-mid").css({
+//             "left": "15px"
+//         });
+//     }
+// });
+
+// $('.carousel').carousel({
+//     interval: 2000
+//   })
